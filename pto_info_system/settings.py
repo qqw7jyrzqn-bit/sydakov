d1b8eae9ea1c092fdc802e6a4ded81d2ff1caa1c
azaner228 <azaner228+bunodalb@bunodalb.digital>
2026-03-11 05:58:49 +0300

    Fix: Use WhiteNoise middleware for jazzmin static files in production

--- pto_info_system/settings.py
+++ pto_info_system/settings.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,11 +162,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# В режиме разработки (DEBUG=True) используем STATICFILES_DIRS
+# В режиме разработки добавляем локальные статические файлы
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static',
     ]
+
+# WhiteNoise для production - используем простое хранилище без манифеста
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
--- pto_info_system/wsgi.py
+++ pto_info_system/wsgi.py
@@ -9,11 +9,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pto_info_system.settings')
 
 application = get_wsgi_application()
-
-# Wrap with WhiteNoise for production static files
-try:
-    from whitenoise import WhiteNoise
-    application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
-    application.add_files(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static'), prefix='static/')
-except ImportError:
-    pass  # WhiteNoise not installed, running in development
